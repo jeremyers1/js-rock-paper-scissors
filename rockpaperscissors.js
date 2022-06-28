08d7dff0cb79f9a3eef95ca2bdee21a8b41cca7b
jeremyers1 <4466264+jeremyers1@users.noreply.github.com>
2022-06-28 15:49:12 -0700

    Add JS textContent methods for played hands

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -2,27 +2,20 @@ let humWins = 0;
 let compWins = 0; 
 let humanRPS = 0;
 
+const choices = document.querySelector('#choices');
+const compScore = document.querySelector('#computerScore');
+const humScore = document.querySelector('#humanScore');
+const winner = document.querySelector('#winner');
 const buttons = document.querySelectorAll('button');
+
+/* example of how:
+const div = document.querySelector('div')
+div.textContent = "My new text!";
+*/
+
 buttons.forEach((button) => {
     button.addEventListener('click', playHand);
 });
-
-function game(hands = 5) {
-    for (let i = 0; i < hands; i++){
-        let c = computerChoice();
-        let h = humanChoice();
-        oneHand(c,h);
-    }
-
-    if (humWins > compWins) {
-        console.log(`You won ${humWins} games and the computer won ${compWins} games. So you beat the computer!`);
-    } else if (humWins < compWins) {
-        console.log(`You won ${humWins} games and the computer won ${compWins} games. So you lost to the computer!`);
-    } else {
-        console.log(`You won ${humWins} games and the computer won ${compWins} games. So you tied! Let's do a tie-breaker.`);
-        game(1);
-    }
-}
 
 function computerChoice() {
     return Math.floor(Math.random() * 3) + 1;
@@ -38,33 +31,35 @@ function playHand() {
 function checkWhoWins(comp, hum) {
     if (comp == 1) {
         if (hum == 1) {
-            console.log("You both chose Rock. It's a tie!");
+            choices.textContent = "You both chose Rock. It's a tie!";
         } else if (hum == 2) {
-            console.log("Computer chose Rock and you chose Paper. You win!");
+            choices.textContent = "Computer chose Rock and you chose Paper. You win!";
             humWins++;
         } else {
-            console.log("Computer chose Rock and you chose Scissors. Computer wins!");
+            choices.textContent = "Computer chose Rock and you chose Scissors. Computer wins!";
             compWins++;
         }
     } else if (comp == 2) {
         if (hum == 2) {
-            console.log("You both chose Paper. It's a tie!");
+            choices.textContent = "You both chose Paper. It's a tie!";
         } else if (hum == 3) {
-            console.log("Computer chose Paper and you chose Scissors. You win!");
+            choices.textContent = "Computer chose Paper and you chose Scissors. You win!";
             humWins++;
         } else {
-            console.log("Computer chose Paper and you chose Rock. Computer wins!");
+            choices.textContent = "Computer chose Paper and you chose Rock. Computer wins!";
             compWins++;
         }
     } else {
         if (hum == 3) {
-            console.log("You both chose Scissors. It's a tie!");
+            choices.textContent = "You both chose Scissors. It's a tie!";
         } else if (hum == 1) {
-            console.log("Computer chose Scissors and you chose Rock. You win!");
+            choices.textContent = "Computer chose Scissors and you chose Rock. You win!";
             humWins++;
         } else {
-            console.log("Computer chose Scissors and you chose Paper. Computer wins!");
+            choices.textContent = "Computer chose Scissors and you chose Paper. Computer wins!";
             compWins++;
         }
     }
 }
+
+
